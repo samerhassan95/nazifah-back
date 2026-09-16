72ceab29b8c2c73dae53c4b9b0af664e5b2bcf0a
samerhassan95 <arch.samerhassan@gmail.com>
2026-09-16 16:02:35 +0300

    fix: make category_id nullable in store service validation

--- Modules/Admin/app/Http/Controllers/AdminLaundryServiceController.php
+++ Modules/Admin/app/Http/Controllers/AdminLaundryServiceController.php
@@ -109,7 +109,7 @@ class AdminLaundryServiceController extends Controller
             'service_description' => 'nullable|array',
             'service_description.ar' => 'nullable|string',
             'service_description.en' => 'nullable|string',
-            'category_id' => 'required|exists:categories,id',
+            'category_id' => 'nullable|exists:categories,id',
         ]);
 
         $serviceData = [
